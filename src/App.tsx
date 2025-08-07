9e0aed6077583837d2ea2c1a815a3e54231ec065
Juan Ignacio Scarafía <juani.scarafia@gmail.com>
2025-08-07 15:43:58 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,8 @@
-// import GlobalStyles from "./styles/GlobalStyles";
-// import styled from "styled-components";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { useState } from "react";
-// import WaterTracker from "./components/WaterTracker";
-// import SettingsPage from "./pages/SettingsPage";
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   height: 100%;
-// `;
-
-// const Content = styled.div`
-//   text-align: center;
-//   max-width: 400px;
-//   width: 100%;
-//   padding: 2rem;
-// `;
-
-// function App() {
-//   const [page, setPage] = useState<'home' | 'settings'>('home');
-
-//   return (
-//     <>
-//       <GlobalStyles />
-//       <Wrapper>
-//         <Content>
-//           {page === 'home' ? (
-//             <WaterTracker onNavigateSettings={() => setPage('settings')} />
-//           ) : (
-//             <SettingsPage onBack={() => setPage('home')} />
-//           )}
-//         </Content>
-//       </Wrapper>
-//       <ToastContainer
-//         position="top-center"
-//         autoClose={3000}
-//         hideProgressBar={false}
-//         newestOnTop
-//         closeOnClick
-//         pauseOnHover
-//         theme="colored"
-//       />
-//     </>
-//   );
-// }
-
-// export default App;
-
 import GlobalStyles from "./styles/GlobalStyles";
 import styled from "styled-components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WaterTracker from "./components/WaterTracker";
 import SettingsPage from "./pages/SettingsPage";
 import HistorialPage from "./pages/HistorialPage"; // nueva página
